c15dcb4ec056f96dc5ef95e48761ce67f2062643
Porter Orloff <porterorloff@gmail.com>
2017-06-10 12:13:06 -0700

    dice roll homework

--- diceroll.py
+++ diceroll.py
@@ -1,14 +1,9 @@
 H=0
-COINFLIPS = 1000
+DICEROLLS = 6
 import random
-for x in range(COINFLIPS):
-	r=random.random()
+for x in range(DICEROLLS):
+	r=random.randint(1,12)
 	print r
-	if r > 0.5:
-		H=H+1
-		print "heads"
-	else:
-		print "tails"
-	print "Number of Heads", H
-	PROB = (H *1.0 / COINFLIPS)
-	print "PROBABILITY", PROB	
+	# print "Number of Heads", H
+	# PROB = (H *1.0 / DICEROLLS)
+	# print "PROBABILITY", PROB
